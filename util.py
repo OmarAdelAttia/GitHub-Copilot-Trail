5550e0019f7272702f748dfceb6c22731acf95a8
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2025-05-03 10:24:43 +0300

    Using AI Powered code suggestions to write code

---
+++ util.py
@@ -0,0 +1,7 @@
+def convert_f_to_c(f):
+    """
+    Convert Fahrenheit to Celsius.
+    :param f: Temperature in Fahrenheit
+    :return: Temperature in Celsius
+    """
+    return (f - 32) * 5 / 9
